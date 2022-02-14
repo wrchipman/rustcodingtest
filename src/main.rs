1644230cfcc9ab35e0ad9a1ffc9456528c5ed6e9
William Chipman <wrchipman@gmail.com>
2022-02-14 14:03:10 -0300

    deposits working

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,17 @@ struct Row<'a> {
 
 #[derive(Debug)]
 #[derive(Deserialize)]
-struct TransactionList {
+struct ApprovedTransaction {
     transaction_id: u32,
-    amount: f32
+    amount: f32,
+    in_dispute: bool
 }
 
 #[derive(Debug)]
 #[derive(Deserialize)]
 struct Client {
     client_id: u16,
-    current_transactions: TransactionList, 
+    current_transactions: Vec<ApprovedTransaction>, 
     available: f32,
     held: f32,
     locked: bool
@@ -38,7 +39,11 @@ fn read_csv(filename: String) -> Result<(), Box<dyn Error>> {
         let row: Row = record.deserialize(None)?;
         process_record(row, &mut clients) 
     }
-    println!("{:?}", clients);
+    println!("client, available, held, total, locked");
+    for client in clients {
+        println!("{},{:.4},{:.4},{:.4},{}", client.client_id.to_string(), client.available, client.held, client.held + client.available, client.locked );
+    }
+    //println!("{:?}", clients);
     Ok(())
 }
 
@@ -67,24 +72,32 @@ fn process_record(row: Row, clients: &mut Vec<Client>) {
         }
         if !client_found {
             if transaction_type == "deposit"{
-                let trans = TransactionList{
+                let approved_trans = ApprovedTransaction{
                     transaction_id: transaction_id,
-                    amount: amount
+                    amount: amount,
+                    in_dispute: false
                 };
-                let new_client = Client {
+                let mut new_client = Client {
                     client_id: client_id,
-                    current_transactions: trans,
                     available: amount,
                     held: 0.0,
-                    locked: false
+                    locked: false,
+                    current_transactions: Vec::new()
                 };
+                new_client.current_transactions.push(approved_trans);
                 clients.push(new_client);
             }            
         }
 }
 
 fn process_deposit(row: Row, client: &mut Client) {
-    
+    let approved_trans = ApprovedTransaction{
+        transaction_id: row.transaction_id,
+        amount: row.amount.parse().unwrap(),
+        in_dispute: false
+    };
+    client.available = client.available + approved_trans.amount;
+    client.current_transactions.push(approved_trans);
 }
 fn process_withdrawal(row: Row, client: &mut Client) {
 
